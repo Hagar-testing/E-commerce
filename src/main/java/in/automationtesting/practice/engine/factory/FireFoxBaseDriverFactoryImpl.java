035a06dfe5254751590364f844c6448672699a6c
Hager <hagar.testing@gmail.com>
2024-08-24 06:46:37 +0300

    Update FireFoxBaseDriverFactoryImpl to include firefoxoptions

--- src/main/java/in/automationtesting/practice/engine/factory/FireFoxBaseDriverFactoryImpl.java
+++ src/main/java/in/automationtesting/practice/engine/factory/FireFoxBaseDriverFactoryImpl.java
@@ -2,12 +2,26 @@ package in.automationtesting.practice.engine.factory;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class FireFoxBaseDriverFactoryImpl implements BaseDriverFactory {
     @Override
     public WebDriver getDriver() {
-        WebDriverManager.firefoxdriver().setup();
-        return new FirefoxDriver();
+        try {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.setCapability("platformName", "Linux");
+            firefoxOptions.addArguments("--headless=new");
+            firefoxOptions.addArguments("--no-sandbox");
+            firefoxOptions.addArguments("--remote-allow-origins=*");
+            return new RemoteWebDriver(new URL("http://localhost:4444"), firefoxOptions);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
